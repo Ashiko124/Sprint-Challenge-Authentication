1861b6b928cdb84f3ecf79274994632d883c649c
Ashiko124 <greeneselectronics@gmail.com>
2018-05-18 10:34:42 -0600

    Got as far as I could don't know about tokens yet

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -1,10 +1,13 @@
 const User = require('../models/userModels');
 const bcrypt = require('bcrypt');
 
-const createUser = (req, res) => {
-  const { username, password } = req.body;
+const createUser = function(req, res) {
+  // const { username, password } = req.body;
+  const user = new User(req.body);
+  user.save().then(user => (res.status(200).send(user))).catch(err => (res.status(500).err))
   // create user takes in the username and password and saves a user.
   // our pre save hook should kick in here saving this user to the DB with an encrypted password.
+  
 };
 
 module.exports = {
